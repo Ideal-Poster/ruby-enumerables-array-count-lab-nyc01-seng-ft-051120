6828b2fca93c0ff2022d0d0d8fd337ce6062543a
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-24 22:49:20 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   binding.pry
-  array.count { |n|  } 
+  array.count { |n| n.class == Sting } 
 end
 
 def count_empty_strings(array)
